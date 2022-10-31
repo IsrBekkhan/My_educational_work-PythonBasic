9b2ca93d770200580561258ec4c4b50975b95108
Bekkhan Israpilov <israpal@bk.ru>
2022-10-31 20:49:16 +0300

    new fix task_7 m16

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,5 +1,6 @@
 amount_skates = int(input('Кол-во коньков: '))
 skates_list = []
+
 for count in range(amount_skates):
     print('Размер', count + 1, end = '')
     skates = int(input('-й пары: '))
@@ -7,6 +8,7 @@ for count in range(amount_skates):
 
 amount_human = int(input('\nКол-во людей: '))
 human_list = []
+
 for count in range(amount_human):
     print('Размер ноги', count + 1, end = '')
     foot_size = int(input('-го человека: '))
@@ -15,9 +17,21 @@ for count in range(amount_human):
 total_human = 0
 
 for size in human_list:
-    for skates in skates_list:
-        if size <= skates:
-            total_human += 1
-            skates_list.remove(skates)
+
+    if skates_list.count(size) != 0:
+        total_human += 1
+        skates_list.remove(size)
+
+    else:
+        temp_list = skates_list[:]
+
+        for _ in range(len(temp_list)):
+            minimum = min(temp_list)
+            if min(temp_list) >= size:
+                total_human += 1
+                skates_list.remove(minimum)
+                break
+            else:
+                temp_list.remove(min(temp_list))
 
 print('\nНаибольшее кол-во людей, которые могут взять ролики:', total_human)
