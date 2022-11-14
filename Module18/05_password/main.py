e85ee4a62e66aae1dca39893b44c15baf48ad5b0
Bekkhan Israpilov <israpal@bk.ru>
2022-11-14 14:48:56 +0300

    fixed task-03, task-05, task-09 from m18

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -6,9 +6,8 @@ while True:
         if symbol.isdigit():
             count += 1
 
-    if password.isupper() or password.islower() or len(password) < 8 or count < 3:
+    if password.islower() or len(password) < 8 or count < 3:
         print('Пароль ненадёжный. Попробуйте ещё раз.')
     else:
         print('Это надёжный пароль!')
         break
-
