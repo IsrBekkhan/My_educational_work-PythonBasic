a832e9089c671edc133cd5a96ffdba04bbedfde6
Bekkhan Israpilov <israpal@bk.ru>
2022-11-17 12:39:55 +0300

    added m21 homework

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def printer(start, num):
+    if start == num:
+        print(num)
+        return
+    print(start)
+    printer(start + 1, num)
+
+
+num = int(input('Введите num: '))
+start = 1
+printer(start, num)
