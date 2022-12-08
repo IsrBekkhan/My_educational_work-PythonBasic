5f62be74fa3cca1c8d9412a5b005295fd000f56a
Bekkhan Israpilov <israpal@bk.ru>
2022-12-08 18:50:26 +0300

    added m25 homework: task-3

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+class MyDict(dict):
+    """
+    Класс MyDict. Родитель: dict
+
+    """
+    def get(self, key):
+        """
+        Метод, возвращающий значение в словаре по его ключу
+        если ключа нет в словаре - возвращает 0.
+
+        :return: значение в словаре
+        или
+        :return int: 0
+        """
+        if key in self:
+            return self[key]
+        return 0
+
+
+# Код для теста
+my_dict = MyDict()
+my_dict[1] = 'Один'
+my_dict[2] = 'Два'
+my_dict[3] = 'Три'
+
+print(my_dict.get(1))
+print(my_dict.get(4))
