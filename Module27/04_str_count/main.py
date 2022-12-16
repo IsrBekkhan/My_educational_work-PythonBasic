63a7c5fa4f1c6477f7e7fdc3dd990cd9a076325c
Bekkhan Israpilov <israpal@bk.ru>
2022-12-16 16:05:00 +0300

    added m27 homework

--- Module27/04_str_count/main.py
+++ Module27/04_str_count/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+from typing import Callable
+from functools import wraps
+
+
+def counter(func: Callable) -> Callable:
+    """
+    Декоратор, считающий и выводящий количество вызовов
+    декорируемой функции.
+    """
+    @wraps(func)
+    def wrapped_func(*args, **kwargs) -> Callable:
+        wrapped_func.counter += 1
+        result = func(*args, **kwargs)
+        print('Фунция {} была вызвана {} раз.'.format(
+            func.__name__, wrapped_func.counter
+        ))
+        return result
+
+    wrapped_func.counter = 0
+    return wrapped_func
+
+
+@counter
+def test() -> None:
+    print('ТЕСТ')
+
+
+test()
+test()
+test()
