381d35247ea39a1a7bc6417c2b069204eb81bf6e
Bekkhan Israpilov <israpal@bk.ru>
2022-12-20 11:51:01 +0300

    added m28 homework

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,39 @@
-# TODO здесь писать код
+class File:
+    """
+    Контекст-менеджер, для работы с файлами.
+
+    Args:
+        file_name (str): имя файла с расширением
+    """
+
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+        self.file_object = None
+
+    def __enter__(self) -> 'file_object':
+        """
+        Магический метод, для подготовки объекта файлового типа.
+
+        :return: объект файлового типа
+        """
+        try:
+            self.file_object = open(self.file_name, 'r', encoding='utf-8')
+        except FileNotFoundError:
+            self.file_object = open(self.file_name, 'w', encoding='utf-8')
+            pass
+
+        return self.file_object
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        """
+        Магический метод, для завершения работы с объектом файлового типа.
+
+        :return: bool
+        """
+        self.file_object.close()
+        return True
+
+
+with File('test.txt') as test:
+    file_object = test
+    print(type(file_object))
