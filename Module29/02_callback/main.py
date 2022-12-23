c6b82402a333f82a23bea0f88aa93e90110c66b3
Bekkhan Israpilov <israpal@bk.ru>
2022-12-23 12:55:01 +0300

    added m29 homework

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+from typing import Callable
+from functools import wraps
+
+
+def callback(user_data: str) -> Callable:
+    def checker(func: Callable) -> Callable:
+
+        @wraps(func)
+        def wrapped_func(*args, **kwargs) -> str:
+            return func()
+        return wrapped_func
+    return checker
+
+
+@callback('//')
+def example() -> str:
+    """
+    Тестовая функция, для проверки работы декоратора.
+    """
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+app = {'//': example}
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
--- Module29/04_singleton/main.py
+++ Module29/04_singleton/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def singleton(cls):
+    """
+    Декоратор Синглтон, гарантирующий, что у класса только один экземпляр.
+    """
+    def wrapped(*args, **kwargs):
+        if not wrapped.instance:
+            wrapped.instance = cls(*args, **kwargs)
+        return wrapped.instance
+    wrapped.instance = None
+    return wrapped
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
