12c835d90f0e45aadfde3363a417eecc030a6d97
Bekkhan Israpilov <israpal@bk.ru>
2022-12-29 11:16:54 +0300

    added m31 homework

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+from re import findall
+
+
+phone_numbers = ['9999999999', '999999-999', '99999x9999']
+rus_counter = ['первый', 'второй', 'третий']
+
+
+for index, number in enumerate(phone_numbers):
+    if findall(r'[89]\d{9}', number) == [number]:
+        print(rus_counter[index], 'номер: всё в порядке')
+    else:
+        print(rus_counter[index], 'номер: не подходит')
